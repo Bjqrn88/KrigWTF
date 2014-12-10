2812518f6637682a2bc4d8f5e348375301d279a5
Bjqrn88 <andersbjorn88@gmail.com>
2014-12-10 12:26:07 +0100

    næsten

---
+++ Krig/Command/ChooseForWarCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Krig.Model;
+
+namespace Krig.Command
+{
+    class ChooseForWarCommand: IUndoRedoCommand
+    {
+        private Cards cards;
+        private bool isSelected;
+
+        public ChooseForWarCommand(Cards _cards, bool _isSelected )
+        {
+            cards = _cards;
+            isSelected = _isSelected;
+        }
+
+        public void Execute()
+        {
+            cards.IsSelected = isSelected;
+        }
+
+        public void UnExecute()
+        {
+            cards.IsSelected = isSelected;
+        }
+    }
+}
